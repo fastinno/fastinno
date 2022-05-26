e1afd74e4ff2fbbfe845369cd55f53abfcdcc185
fakhrip <fakhrip@protonmail.com>
2022-05-27 06:13:44 +0700

    feature: add font and accent color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,27 @@
+const defaultTheme = require("tailwindcss/defaultTheme")
+
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "serif": ["Grenze", ...defaultTheme.fontFamily.serif],
+      },
+
+      colors: {
+        "accent": {
+          100: "#d6dbe9",
+          200: "#adb8d2",
+          300: "#8594bc",
+          400: "#5c71a5",
+          500: "#334d8f",
+          600: "#293e72",
+          700: "#1f2e56",
+          800: "#141f39",
+          900: "#0a0f1d"
+        },
+      }
+    },
   },
   plugins: [],
 }
