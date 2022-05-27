0c7b215774887a926773949b36190f7918da1f31
fakhrip <fakhrip@protonmail.com>
2022-05-27 08:37:00 +0700

    feature: add transitions and polish some structure

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,19 @@
-document.addEventListener('alpine:init', () => {
-    Alpine.data('application', () => ({
+// Initialize the applications data when alpine is currently initializing itself
+document.addEventListener("alpine:init", () => {
+    Alpine.data("application", () => ({
         menus: [
             "About",
             "Portfolio",
             "Solutions",
             "Contact Us"
         ],
-    }))
-})
+
+        isScrolled: false,
+    }));
+});
+
+// Remove placeholder layout when alpine is already initialized
+document.addEventListener("alpine:initialized", () => {
+    let cover = document.getElementById("cover");
+    cover.remove();
+});
